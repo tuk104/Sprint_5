93cf8d376bb3f4478fcd3d4bfb7d5a1a058e3800
Pavel Andreev <pashka.98@yandex.ru>
2026-06-14 13:54:11 +0300

    Fix: replace existing_user_data fixture with EXISTING_USER constant

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,11 +2,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import *
 from conftest import driver, registered_user
+from data import *
 
 
 class TestLogin:
     
-    def test_login_via_main_button(self, driver, existing_user_data):
+    def test_login_via_main_button(self, driver):
         
         # Клик по кнопке "Войти в аккаунт"
         driver.find_element(*LOGIN_MAIN_BUTTON).click()
@@ -15,8 +16,8 @@ class TestLogin:
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)
         )
-        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(existing_user_data["email"])
-        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(existing_user_data["password"])
+        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(EXISTING_USER["email"])
+        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(EXISTING_USER["password"])
         driver.find_element(*LOGIN_BUTTON).click()
         
         # Проверка: после входа появилась кнопка "Оформить заказ"
@@ -25,7 +26,7 @@ class TestLogin:
         )
         assert driver.find_element(*CHECKOUT_BUTTON).is_displayed()
     
-    def test_login_via_personal_account_button(self, driver, existing_user_data):
+    def test_login_via_personal_account_button(self, driver):
         # Клик по "Личный кабинет"
         driver.find_element(*PERSONAL_ACCOUNT_BUTTON).click()
         
@@ -33,8 +34,8 @@ class TestLogin:
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)
         )
-        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(existing_user_data["email"])
-        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(existing_user_data["password"])
+        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(EXISTING_USER["email"])
+        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(EXISTING_USER["password"])
         driver.find_element(*LOGIN_BUTTON).click()
         
         # Проверка: успешный вход
@@ -43,7 +44,7 @@ class TestLogin:
         )
         assert driver.find_element(*CHECKOUT_BUTTON).is_displayed()
     
-    def test_login_via_register_form(self, driver, existing_user_data):
+    def test_login_via_register_form(self, driver):
         # Переход на страницу регистрации
         driver.find_element(*PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 5).until(
@@ -59,8 +60,8 @@ class TestLogin:
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)
         )
-        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(existing_user_data["email"])
-        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(existing_user_data["password"])
+        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(EXISTING_USER["email"])
+        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(EXISTING_USER["password"])
         driver.find_element(*LOGIN_BUTTON).click()
         
         # Проверка: успешный вход
@@ -69,7 +70,7 @@ class TestLogin:
         )
         assert driver.find_element(*CHECKOUT_BUTTON).is_displayed()
     
-    def test_login_via_forgot_password_form(self, driver, existing_user_data):
+    def test_login_via_forgot_password_form(self, driver):
         # Переход на страницу восстановления пароля
         driver.find_element(*PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 5).until(
@@ -85,8 +86,8 @@ class TestLogin:
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)
         )
-        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(existing_user_data["email"])
-        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(existing_user_data["password"])
+        driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(EXISTING_USER["email"])
+        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(EXISTING_USER["password"])
         driver.find_element(*LOGIN_BUTTON).click()
         
         # Проверка: успешный вход
